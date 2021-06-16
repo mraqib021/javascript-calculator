82aa81a7b73fcdaf48374864c692e078ba85a5b9
aqib <mraqib021@gmail.com>
2021-06-17 00:57:00 +0500

    version 1.2

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-function display(value){
-    let res = document.getElementById("result")
-    res.value += value
+function setvalue(num){
+    //console.log(num)
+    let val = document.getElementById("display1")
+    var lastVal = val.value.slice(val.value.length - 1);
+    var opt = ['+','-','*','/'];
+    if(opt.indexOf(lastVal) !== -1 && opt.indexOf(num) !== -1){
+        val.value = val.value.slice(0,-1) + num
+    }else{
+        val.value += num;
+    }
 }
-function removescreen(){
-    document.getElementById("result").value=""
+function equaltoo(eql){
+    let val = document.getElementById("display1")
+    val.value = eval(val.value)
+    //console.log(eval(val.value))
+}
+function remove(empty){
+    let val = document.getElementById("display1")
+    val.value = empty
+    //console.log(val.value)
+}
+function del(minus1){
+    let val = document.getElementById("display1")
+    val.value = val.value.slice(0,val.value.length -1)
+    console.log(val.value.length -1)
 }
